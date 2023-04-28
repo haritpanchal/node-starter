104b7eaefc96e9b70425a4c2eb61cdabeb62278d
Harit.Panchal <panchalharit007@gmail.com>
2023-04-28 14:04:56 +0530

    controllers added

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const errorControllers = require('./controllers/error')
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle:'404 Not Found'})
-});
+app.use(errorControllers.get404);
 
-app.listen(3002);
+app.listen(3000);
